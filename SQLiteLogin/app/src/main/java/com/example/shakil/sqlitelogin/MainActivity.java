1f7a140e3019fe6af5432deb662a9ecf17345f4e
android_shakil <shakhawathossen53@gmail.com>
2019-01-26 00:54:10 +0600

    Create SignUpActivity and design it

--- SQLiteLogin/app/src/main/java/com/example/shakil/sqlitelogin/MainActivity.java
+++ SQLiteLogin/app/src/main/java/com/example/shakil/sqlitelogin/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.shakil.sqlitelogin;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.shakil.sqlitelogin.Database.MyDatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     MyDatabaseHelper myDatabaseHelper;
+
+    private EditText edtUserName, edtPassword;
+    private Button btnSignIn, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
         myDatabaseHelper = new MyDatabaseHelper(this);
         SQLiteDatabase sqLiteDatabase = myDatabaseHelper.getWritableDatabase();
+
+        edtUserName = findViewById(R.id.edtUserName);
+        edtPassword = findViewById(R.id.edtPassword);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userName = edtUserName.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                if (v.getId() == R.id.btnSignIn){
+
+                }
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.btnSignUp){
+                    startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                }
+            }
+        });
     }
 }
